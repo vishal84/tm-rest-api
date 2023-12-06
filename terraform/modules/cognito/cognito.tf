a185c7eed0d7aaab7b598a2dd6e62f01a126bb2d
Vishal Patel <5624846+vishal84@users.noreply.github.com>
2023-12-06 14:58:03 -0500

    add: cognito operations user pool

--- terraform/modules/cognito/cognito.tf
+++ terraform/modules/cognito/cognito.tf
@@ -1,5 +1,85 @@
 # Create a Cognito pool for Saas Ops users (Admin, Support, etc)
-resource "aws_cognito_user_pool" "operations_user_pool" {
-  name = "operations"
-}
+data "aws_caller_identity" "current" {}
 
+resource "aws_cognito_user_pool" "operations_user_pool" {
+  name = "operations-users"
+
+  auto_verified_attributes = ["email"]
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+  }
+
+  admin_create_user_config {
+    invite_message_template {
+      email_subject = "Welcome! A new operations user has been created."
+      email_message = templatefile("${path.module}/templates/operations_user_signup.tftpl", {
+
+      })
+      # sms_message = "Your username is {username} and password is {####}."
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "email"
+    required            = true
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "role"
+    required            = false
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "tenantId"
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name                = "apiKey"
+    required            = false
+    mutable             = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+} # aws_cognito_user_pool.operations_user_pool
+
+resource "aws_cognito_user_pool_client" "client" {
+  name                                 = "operations-client"
+  allowed_oauth_flows_user_pool_client = true
+  generate_secret                      = false
+  allowed_oauth_scopes                 = ["aws.cognito.signin.user.admin", "email", "openid", "profile"]
+  allowed_oauth_flows                  = ["implicit", "code"]
+  explicit_auth_flows                  = ["ADMIN_NO_SRP_AUTH", "USER_PASSWORD_AUTH"]
+  supported_identity_providers         = ["COGNITO"]
+
+  user_pool_id  = aws_cognito_user_pool.pool.id
+  callback_urls = ["https://example.com"]
+  logout_urls   = ["https://sumeet.life"]
+}
